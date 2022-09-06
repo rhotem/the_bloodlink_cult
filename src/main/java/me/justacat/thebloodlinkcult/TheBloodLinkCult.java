6ae9b8f8cdeaa294bcda27df786a447752f2c8cd
Rhotem <Rhotem.alonreshef@gmail.com>
2022-09-06 11:35:36 +0300

    fix

--- src/main/java/me/justacat/thebloodlinkcult/TheBloodLinkCult.java
+++ src/main/java/me/justacat/thebloodlinkcult/TheBloodLinkCult.java
@@ -2,6 +2,8 @@ package me.justacat.thebloodlinkcult;
 
 import me.justacat.thebloodlinkcult.commands.GetItemCommand;
 import me.justacat.thebloodlinkcult.commands.GetItemTabComplete;
+import me.justacat.thebloodlinkcult.items.CustomItem;
+import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TheBloodLinkCult extends JavaPlugin {
@@ -12,6 +14,9 @@ public final class TheBloodLinkCult extends JavaPlugin {
     public void onEnable() {
 
         instance = this;
+
+        CustomItem.customItemKey = new NamespacedKey(this, "CustomItem");
+        CustomItem.uuidKey = new NamespacedKey(this, "DrillUUID");
 
         getCommand("GetItem").setExecutor(new GetItemCommand());
         getCommand("GetItem").setTabCompleter(new GetItemTabComplete());
